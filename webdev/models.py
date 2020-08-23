c12545fc815642fe8611077e2605ccdb6e56ad67
Saad Shami <sshami91@gmail.com>
2020-08-23 17:15:48 -0400

    Created web project detail page model

--- webdev/models.py
+++ webdev/models.py
@@ -5,8 +5,10 @@ from wagtail.core.fields import StreamField
 from wagtail.core.blocks import PageChooserBlock, StreamBlock
 from wagtail.images.blocks import ImageChooserBlock
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.admin.edit_handlers import PageChooserPanel
 
 
+# Web Development Homepage
 class Webdev(Page):
     page_description = models.CharField(max_length=255)
     featured_projects = StreamField([
@@ -23,6 +25,7 @@ class Webdev(Page):
     ]
 
 
+# Web Project item (used for StreamField)
 class WebProject(blocks.StructBlock):
     demo_laptop_display = ImageChooserBlock(required=True)
     demo_mobile_display = ImageChooserBlock(required=False)
@@ -31,6 +34,7 @@ class WebProject(blocks.StructBlock):
         icon = 'image'
 
 
+# Standard Project item (used for StreamField)
 class StandardProject(blocks.StructBlock):
     demo_display = ImageChooserBlock(required=True)
 
@@ -38,6 +42,7 @@ class StandardProject(blocks.StructBlock):
         icon = 'image'
 
 
+# Web Development Project Listing
 class WebdevProject(Page):
     summary = models.TextField()
     display_image = StreamField(
@@ -56,4 +61,44 @@ class WebdevProject(Page):
 
     parent_page_type = [
         'webdev.Webdev'
+    ]
+
+
+# Single Image with Caption
+class SingleImageBlock(blocks.StructBlock):
+    image = ImageChooserBlock(required=True)
+    caption = blocks.TextBlock(required=False)
+
+    class Meta:
+        icon = 'image'
+
+
+# Double Image with Caption
+class DoubleImageBlock(blocks.StructBlock):
+    image_left = ImageChooserBlock(required=True)
+    image_right = ImageChooserBlock(required=True)
+    caption = blocks.TextBlock(required=False)
+
+    class Meta:
+        icon = 'image'
+
+
+# Project Detail Page
+class ProjectDetailPage(Page):
+    project_listing = models.ForeignKey(
+        'wagtailcore.Page',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
+    body = StreamField([
+        ('paragraph', blocks.RichTextBlock()),
+        ('single_image', SingleImageBlock()),
+        ('double_image', DoubleImageBlock())
+    ])
+
+    content_panels = Page.content_panels + [
+        PageChooserPanel('project_listing', 'webdev.WebdevProject'),
+        StreamFieldPanel('body')
     ]
